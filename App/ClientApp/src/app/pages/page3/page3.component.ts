58b5b2a49073b658da85d6e56ca25bc02b6d14f3
Jigar <jkh@narola.email>
2019-03-29 18:21:59 +0530

    - Changes

--- App/ClientApp/src/app/pages/page3/page3.component.ts
+++ App/ClientApp/src/app/pages/page3/page3.component.ts
@@ -3,6 +3,9 @@ import { pageSlideUpAnimation } from '../../misc/page.animation';
 import { BaseApiService } from 'src/app/services/baseapiservice';
 import { Query, Sort, Paginator } from '../../misc/query';
 import { ProductViewModel } from './productViewModel';
+import { filter } from 'rxjs/operators';
+import { isNumber } from 'util';
+import { Operator } from 'src/app/misc/app.enums';
 
 @Component({
     selector: 'page-three',
@@ -15,6 +18,11 @@ export class Page3Component implements OnInit {
     paginator: Paginator = new Paginator(1, 5);
     data: Array<ProductViewModel> = [];
     loading = false;
+    filter: any = {
+        name: null,
+        pricegt: null,
+        pricelt: null
+    };
 
     constructor(private apiService: BaseApiService) {
     }
@@ -25,6 +33,19 @@ export class Page3Component implements OnInit {
     getProducts() {
         let q: Query = new Query(this.paginator.pageNo, this.paginator.pageSize);
         q.addSort(this.sort.columnName, this.sort.direction);
+
+        if (this.filter.name)
+            q.addCondition("ProductName", this.filter.name.trim(), Operator.StartsWith);
+
+        if (this.filter.pricegt && isNumber(Number(this.filter.pricegt))) {
+            q.addAnd();
+            q.addCondition("UnitPrice", Number(this.filter.pricegt.trim()), Operator.Ge);
+        }
+        if (this.filter.pricelt && isNumber(Number(this.filter.pricelt))) {
+            q.addAnd();
+            q.addCondition("UnitPrice", Number(this.filter.pricelt.trim()), Operator.Le);
+        }
+
         this.loading = true;
         this.apiService.getByParams("api/product/list", { q: q }).subscribe(result => {
             this.paginator.totalItems = result.data.total;
